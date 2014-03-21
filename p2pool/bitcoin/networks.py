948982e3f38e3e2745056abe323b501e0e89074a
Allan Andal <aandal@fairfaxmedia.com.au>
2014-03-21 17:27:51 +1100

    add subrosa net

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -145,6 +145,49 @@ nets = dict(
         DUST_THRESHOLD=1e8,
     ),
 
+    subrosa=math.Object(
+        P2P_PREFIX='fbc0b6db'.decode('hex'),
+        P2P_PORT=9333,
+        ADDRESS_VERSION=48,
+        RPC_PORT=9332,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'subrosaaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//840000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=150, # s
+        SYMBOL='LTC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Subrosa') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Subrosa/') if platform.system() == 'Darwin' else os.path.expanduser('~/.subrosa'), 'subrosa.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.subrosa.net/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.subrosa.net/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.subrosa.net/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
+    subrosa_testnet=math.Object(
+        P2P_PREFIX='fcc1b7dc'.decode('hex'),
+        P2P_PORT=19333,
+        ADDRESS_VERSION=111,
+        RPC_PORT=19332,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'subrosaaddress' in (yield bitcoind.rpc_help()) and
+            (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//840000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=150, # s
+        SYMBOL='tLTC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Subrosa') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Subrosa/') if platform.system() == 'Darwin' else os.path.expanduser('~/.subrosa'), 'subrosa.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://nonexistent-subrosa-testnet-explorer/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://nonexistent-subrosa-testnet-explorer/address/',
+        TX_EXPLORER_URL_PREFIX='http://nonexistent-subrosa-testnet-explorer/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=1e8,
+    ),
+
     terracoin=math.Object(
         P2P_PREFIX='42babe56'.decode('hex'),
         P2P_PORT=13333,
